1a5ca2fdccd7677995cb0dd0abf70d0c2ed2e383
ElouanV <elouan.vincent@epita.fr>
2020-03-06 17:16:35 +0100

    Inventory + entitystat

---
+++ Kamel Quest/Assets/Scenes/UnitStat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    enum EntityType
+    {
+        PLAYER,
+        ENEMY,
+        PNJ
+    }
+    private List<Monster> team;
+    private List<Item> inventory;
+    private int lvl;
+    public int Lvl => get;
+
+
+    public Entity ()
+    {
+        this.team = new List<Monster>;
+        inventory = new List<item>;
+        this.lvl = lvl
+    }
+    public
+}
