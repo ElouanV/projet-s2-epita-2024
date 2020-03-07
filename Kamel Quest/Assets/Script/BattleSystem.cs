6c3a4a4d75c0a6d1b51d578c60149d723e332159
OmbreAstrale91 <49863003+OmbreAstrale91@users.noreply.github.com>
2020-03-08 00:42:21 +0100

    battle system test
    a finir

---
+++ Kamel Quest/Assets/Script/BattleSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum BattleState
+{
+    START,
+    PLAYERTURN,
+    ENEMYTURN,
+    WIN,
+    LOSE
+}
+public class BattleSystem : MonoBehaviour
+{
+    public BattleState state;
+    public Text dialogue;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+    }
+
+    public void SetupBattle() //faire spawn les entités au bonne endroit et mettre les UI a jour
+    {
+        state = BattleState.PLAYERTURN;
+    }
+
+   
+
+    
+
+    public void AttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+
+        
+    }
+    
+    
+    
+    
+
+ 
+}
