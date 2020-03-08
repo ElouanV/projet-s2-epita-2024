9c42955d19a8c58a4b9b9e6bf78f0c840b56cb77
Julien Mayer <julien.mayer@epita.fr>
2020-03-08 22:13:22 +0100

    cette fois c'est fait les animation !

---
+++ Kamel Quest/Assets/Script/PlayerControler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float moveSpeed;
+    public Animator anim;
+    public bool moving;
+    private Vector2 lastMove;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moving = false;
+        if (Input.GetAxisRaw("Horizontal") > 0f ||  Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            moving = true;
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0f || Input.GetAxisRaw("Vertical") < 0f)
+        {
+            moving = true;
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("Moving", moving);
+		anim.SetFloat("LastMoveX",lastMove.x);
+		anim.SetFloat("LastMoveY",lastMove.y);
+
+
+    }
+}
