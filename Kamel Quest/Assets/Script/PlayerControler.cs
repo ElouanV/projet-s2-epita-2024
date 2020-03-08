d26642b262fee769cdb115e3e3c067bca08e9afd
Julien Mayer <julien.mayer@epita.fr>
2020-03-08 19:52:24 +0100

    Player ctrl

--- Kamel Quest/Assets/Script/PlayerControler.cs
+++ Kamel Quest/Assets/Script/PlayerControler.cs
@@ -5,20 +5,40 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     public float moveSpeed;
-    
+    public Animator anim;
+    public bool moving;
+    private Vector2 lastMove;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0.5f ||  Input.GetAxisRaw("Horizontal") < -0.5f)
+        moving = false;
+        if (Input.GetAxisRaw("Horizontal") > 0f ||  Input.GetAxisRaw("Horizontal") < 0f)
         {
+            moving = true;
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        if (Input.GetAxisRaw("Vertical") > 0f || Input.GetAxisRaw("Vertical") < 0f)
         {
+            moving = true;
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+
         }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("Moving", moving);
+		anim.SetFloat("LastMoveX",lastMove.x);
+		anim.SetFloat("LastMoveY",lastMove.y);
+
 
     }
 }
