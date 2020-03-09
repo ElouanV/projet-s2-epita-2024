0733df33a86f150672bd04b6c1d624a9398f37dd
Martin SANTORO <martin.santoro@epita.fr>
2020-03-09 21:08:07 +0100

    Vuala

---
+++ Kamel Quest/Assets/Scenes/Martin/Dialogue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.UI;
+
+
+public class Dialogue : MonoBehaviour
+{
+    public GameObject Bubble;
+    public bool isTrigger;
+
+    void OnTriggerEnter2D()
+    {
+        isTrigger = true;
+        Bubble.SetActive(isTrigger);
+    }
+
+    void OnTriggerExit2D()
+    {
+        isTrigger = false;
+        Bubble.SetActive(isTrigger);
+    }
+}
+
