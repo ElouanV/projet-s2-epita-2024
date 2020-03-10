0359e669c4164631be7799b5dca57cca6c28915f
Julien Mayer <julien.mayer@epita.fr>
2020-03-10 01:50:29 +0100

    text on the bubble

---
+++ Kamel Quest/Assets/Script/ShowsText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowsText : MonoBehaviour
+{
+    public string[] sentences;
+    public float speed = 0.2f;
+
+    public bool restart;
+    private TextMesh Txt;
+    private string first_sentence;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Txt = transform.GetChild(0).GetComponent<TextMesh>();
+        first_sentence = sentences[0];
+        StartCoroutine(ShowsTxt());
+    }
+    // OnEnable is called every time this GameObject is Actived
+    void OnEnable()
+    {
+        StartCoroutine(ShowsTxt());
+    }
+
+    IEnumerator ShowsTxt()
+    {
+        Txt.text = "";
+        string tmp = first_sentence;
+        int nb_char = first_sentence.Length;
+        
+        for (int i = 1; i <= nb_char; i++)
+        {
+            yield return new WaitForSeconds(speed);
+            Txt.text = tmp.Substring(0, i);
+        }
+        
+        
+    }
+}
