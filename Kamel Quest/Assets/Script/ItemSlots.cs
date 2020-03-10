aa658078f492f544d662147af0835b31d31c3475
OmbreAstrale91 <49863003+OmbreAstrale91@users.noreply.github.com>
2020-03-10 23:53:57 +0100

    Revert "Merge branch 'master' of https://github.com/ElouanV/projet-s2-epita-2024"
    This reverts commit 173cdb35a8e4bf9d73aebf9293ba20738644684c, reversing
    changes made to c96338c8179eb0fbcd02c3ce23d675b3d6f1d237.

--- Kamel Quest/Assets/Script/ItemSlots.cs
+++ Kamel Quest/Assets/Script/ItemSlots.cs
@@ -8,11 +8,10 @@ public class ItemSlots : MonoBehaviour
     public Text textItem;
     public GameObject textDisplay;
     [Header("Item's Data")]
-    public string itemName;
+    public string itemType;
     public string itemID;
     public Sprite itemSprite;
     public string itemDescription;
-    public int count;
     // Start is called before the first frame update
     void Start()
     {
