5fb12d5eaea5fc7861d47d5506237356a8569f2b
Julien Mayer <julien.mayer@epita.fr>
2020-03-10 03:36:04 +0100

    animation texte

--- Kamel Quest/Assets/Script/ShowsText.cs
+++ Kamel Quest/Assets/Script/ShowsText.cs
@@ -4,38 +4,46 @@ using UnityEngine;
 
 public class ShowsText : MonoBehaviour
 {
-    public string[] sentences;
+    public string[] sentencesArr;
     public float speed = 0.2f;
-
+    public float timeForRead = 10f;
+    public bool scroll;
     public bool restart;
     private TextMesh Txt;
     private string first_sentence;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         Txt = transform.GetChild(0).GetComponent<TextMesh>();
-        first_sentence = sentences[0].Replace("$","\n");
-        StartCoroutine(ShowsTxt());
-    }
-    // OnEnable is called every time this GameObject is Actived
-    void OnEnable()
-    {
-        StartCoroutine(ShowsTxt());
+        string sentence = sentencesArr[0].Replace("$", "\n");
+        if (scroll) StartCoroutine(ShowsTxt(sentence));      
+        else Txt.text = sentence;
     }
 
-    IEnumerator ShowsTxt()
+// OnEnable is called every time this GameObject is Actived
+    void OnEnable()
+    {
+        Start();
+    }
+
+    void Upddate()
+    {
+     
+    }
+
+    IEnumerator ShowsTxt(string sentence)
     {
         Txt.text = "";
-        string tmp = first_sentence;
-        int nb_char = first_sentence.Length;
+        int nb_char = sentence.Length;
         
         for (int i = 1; i <= nb_char; i++)
         {
+            if (Input.GetKeyDown(KeyCode.Space)) break;
             yield return new WaitForSeconds(speed);
-            Txt.text = tmp.Substring(0, i);
+            Txt.text = sentence.Substring(0, i);
         }
-        
+        Txt.text = sentence;
         
     }
 }
