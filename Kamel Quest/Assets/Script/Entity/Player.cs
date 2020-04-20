5b80f91efe1f9a88bdd871b0e8f0b2ec685fcdfc
Martin4242 <55755801+Martin4242@users.noreply.github.com>
2020-04-20 21:33:01 +0200

    Bouton upgradeV1
    Bouton upgradeV1

--- Kamel Quest/Assets/Script/Entity/Player.cs
+++ Kamel Quest/Assets/Script/Entity/Player.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class Player : Entity
 {
     public int argent;
+    public int armurelvl;
+    public int epeelvl;
+    public int bouclierlvl;
+    public int casquelvl;
 
-    public Player(long hpmax,  int atk, string name, int magicatk, int atkcost, int argent)
+
+    public Player(long hpmax,  int atk, string name, int magicatk, int atkcost, int argent, int armurelvl, int epeelvl, int bouclierlvl, int casquelvl)
     {
+        this.argent = argent;
+        this.currenthp = hpmax;
+        this.atkcost = atkcost;
+        this.magicatk = magicatk;
         this.hpmax = hpmax;
         this.atk = atk;
         this.name = name;
-        this.magicatk = magicatk;
-        this.atkcost = atkcost;
-        this.currenthp = hpmax;
-        this.argent = argent;
+        //Equipement Automatiquement à 0
+        this.armurelvl = armurelvl;
+        this.epeelvl = epeelvl;
+        this.bouclierlvl = bouclierlvl;
+        this.casquelvl = casquelvl;
     }
     // Start is called before the first frame update
     void Start()
