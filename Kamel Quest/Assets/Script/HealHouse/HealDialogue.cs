422497ed8a53d16c27ef2d7aa88e7a9d4038e6b3
Martin4242 <55755801+Martin4242@users.noreply.github.com>
2020-05-18 19:26:05 +0200

    HealHouse V1
    HealHouse V1

---
+++ Kamel Quest/Assets/Script/HealHouse/HealDialogue.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealDialogue : MonoBehaviour
+{
+
+    public Player  player;
+    
+	public bool is_trigger;
+    
+    void Update()
+    {
+        if (is_trigger && Input.GetKeyUp(KeyCode.Space))
+        {
+            HealTeam();
+        }
+    }
+
+
+	// Display the bubble is the player is next to the PNG
+    void OnTriggerEnter2D()
+    {
+		is_trigger = true;
+		Debug.Log("[HealDialogue] OnTriggerEnter2D: HealTeam ");
+    }
+
+    // Close the bubble when the player goes away
+	void OnTriggerExit2D()
+    {
+		is_trigger = false;
+    }
+
+    void HealTeam()
+    {
+        foreach (Ally allier in player.team)
+        {
+            allier.currenthp = allier.hpmax;
+        }
+    }
+}
