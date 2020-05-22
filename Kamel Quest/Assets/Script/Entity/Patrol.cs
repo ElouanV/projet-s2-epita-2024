d7ac8d226ed5c9816746380baff814a32351745e
OmbreAstrale91 <49863003+OmbreAstrale91@users.noreply.github.com>
2020-05-22 14:47:32 +0200

    Spawn et mouvement monstre
    sisi la cité

---
+++ Kamel Quest/Assets/Script/Entity/Patrol.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Patrol : MonoBehaviour
+{
+    //vitesse et différentes positions possibles pour l'entité
+    public int speed;
+    public Transform[] patrolPosition;
+    public int randomPosition;
+    
+    //temps d'attente
+    private float waitAtSpot;
+    public float startWaitAtSpot;
+
+    private void Start()
+    {
+        randomPosition = Random.Range(0, patrolPosition.Length);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, patrolPosition[randomPosition].position,
+            speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, patrolPosition[randomPosition].position) < 0.5f)
+        {
+            if (waitAtSpot > 0)
+            {
+                waitAtSpot -= Time.deltaTime;
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, patrolPosition[randomPosition].position,
+                    speed * Time.deltaTime);
+                waitAtSpot = startWaitAtSpot;
+            }
+        }
+    }
+}
